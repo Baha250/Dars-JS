b6a4799c7786aebb7620952d644a3cea36fa14b7
Bahriddin <bahriddin25@gmail.com>
2021-02-25 09:16:08 +0500

    first commit

---
+++ lesson.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+console.log(5 + + "5");
+
+let incr = 15,
+    decr = 15;
+    ++incr;
+    --decr;
+console.log(incr);
+console.log(decr);
+console.log(11%3);
+console.log(5*3 === 15);
+const checked = false;
+const unchecked = false;
+console.log(checked || !unchecked);
+console.log(2 + 2 * 2 != 8);
